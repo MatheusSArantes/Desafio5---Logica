ee743b0eaec271b6e34df6c55738c6ba1ca6716e
“MatheusSArantes” <matheus.sodre.arantes@gmail.com>
2021-05-28 10:54:40 -0300

    Desafio5

---
+++ mod.js
@@ -0,0 +1,10 @@
+var lista = ['item1','item2','item3','item4','item5'];
+lista.unshift('item0')
+lista.pop('item5')
+lista.push('item6','item7')
+lista.shift('item0')
+console.log(lista)
+
+var numeros = [7,5,6,3,8,9,2,1,4]
+numeros.sort()
+console.log(numeros)
